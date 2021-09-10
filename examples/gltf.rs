db63da3292235f88fcccf5821e5f1a27ea1868c7
Fedor Logachev <not.fl3@gmail.com>
2021-09-10 11:01:16 -0500

    fix example

--- examples/gltf.rs
+++ examples/gltf.rs
@@ -2,15 +2,25 @@ use macroquad::prelude::*;
 
 #[macroquad::main("3D")]
 async fn main() {
-    let model = load_model("../lowres/assets/ship.gltf").await.unwrap();
+    let model = load_model("examples/ship.gltf").await.unwrap();
 
     loop {
-        clear_background(LIGHTGRAY);
+        let render_state = RenderState {
+            camera: Camera::Camera2D {
+                rotation: 0.,
+                zoom: vec2(1. / 400., -1. / 400.),
+                target: vec2(200., 200.),
+                offset: vec2(0., 0.),
+            },
+            ..Default::default()
+        };
 
-        let mut canvas = scene_graph().sprite_layer(&Default::default());
+        scene_graph().clear2(&render_state, Color::new(0.2, 0.2, 0.5, 1.0));
+
+        let mut canvas = scene_graph().sprite_layer(&render_state);
         draw_text(&mut canvas, "WELCOME TO 3D WORLD", 10.0, 20.0, 30.0, BLACK);
-        draw_text(&mut canvas, "TEXT BELOW!!!", 400.0, 600.0, 30.0, BLUE);
-        draw_rectangle(&mut canvas, 300., 300., 100., 100., RED);
+        draw_text(&mut canvas, "TEXT BELOW!!!", 400.0, 400.0, 30.0, BLUE);
+        draw_rectangle(&mut canvas, 300., 200., 100., 100., RED);
         scene_graph().draw_canvas(canvas);
 
         scene_graph().draw_model(
@@ -28,8 +38,9 @@ async fn main() {
             Mat4::IDENTITY,
         );
 
-        let mut canvas = scene_graph().sprite_layer(&Default::default());
-        draw_rectangle(&mut canvas, 500., 600., 100., 100., GREEN);
+        let mut canvas = scene_graph().sprite_layer(&render_state);
+
+        draw_rectangle(&mut canvas, 100., 350., 100., 100., GREEN);
         draw_text(&mut canvas, "TEXT ABOVE!!!", 400.0, 300.0, 30.0, YELLOW);
         scene_graph().draw_canvas(canvas);
 
